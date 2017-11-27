eef09794d4739375b8a7ef87b5a87771ab9c370b
Ludwig Kleiser <ludde_orby@hotmail.com>
2017-11-27 18:18:14 +0100

    Added all functions except background color function

--- WebbApi/WebbApi/Controllers/DataBindingController.cs
+++ WebbApi/WebbApi/Controllers/DataBindingController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Drawing;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,10 +12,36 @@ namespace WebbApi.Controllers
     [Route("api/databinding")]
     public class DataBindingController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        [HttpGet, Route("breakfast")]
+        public IActionResult Breakfast(string input)
         {
-            return View();
+            if (input.ToLower() == "ägg") return Ok($"Å nej, du borde inte äta {input} till frukost!");
+            return Ok($"Ja, {input} är gott! ");
+
+        }
+
+        [HttpGet, Route("square")]
+        public IActionResult Square(int value)
+        {
+            return Ok($"{value} * {value} = {value * value}");
+        }
+
+        [HttpGet, Route("listOfNumbers")]
+        public IActionResult listOfNumbers(int numbrFrom, int numbrTo)
+        {
+           
+
+            var numbers =
+              Enumerable
+              .Range(numbrFrom, numbrTo)
+              .ToArray();
+            return Ok(numbers);
+        }
+
+        [HttpGet,Route("colorPicker")]
+        public IActionResult ColorPicker(string color)
+        {
+            
         }
     }
 }
